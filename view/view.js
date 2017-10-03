19ec97923b8555f579717c21e4be270f846dcf15
Raghavendra Udyavar <raghav@ksubaka.com>
2017-10-04 07:53:40 +0800

    [feature]practising git squash so that I can greet someone well in chinese next time

---
+++ view/view.js
@@ -0,0 +1,7 @@
+function viewExtension() {
+    Console.log("Lets see what amber has here");
+}
+
+view.bind({
+    visisble(){Console.log("I m visible here")}, hide (){Console.log("I am not visible ehre")}
+});
